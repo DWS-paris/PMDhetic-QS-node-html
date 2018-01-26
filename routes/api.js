1300d289d97a7b77607da84d62382658274f8692
Julien Noyer <dwsparis@MacBook-Pro-de-Julien.local>
2018-01-26 14:09:12 +0100

    Ajout de Mongoose dans l'API

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,12 @@ Importer les composants de la route
     const bodyParser = require('body-parser');
 //
 
+/*
+Configuration de Mongoose
+*/
+    const mongoose = require('mongoose');
+    const mongoServeur = 'mongodb://localhost:27018/blog';
+//
 
 /*
 Configuration de body-parser
@@ -24,9 +30,31 @@ Définition des routes
     });
 
     // Afficher la liste des posts
-    router.get( '/tasks', (req, res) => {
-        // Renvoyer un flux JSON dans la réponse
-        res.json( { content: 'Hello API' } );
+    router.get( '/posts', (req, res) => {
+       
+        // Connexion à la BDD MongoDB
+        mongoose.connect( mongoServeur, ( err, db ) => {
+
+            // Tester la connexion à la BDD
+            if( err ) { res.json({ error: err }) }
+            else {
+
+                // Connexion ouverte : récupérer la collection de données
+                db.collection('posts').find().toArray( (err, collection) => {
+
+                    // Tester la connexion à la collection
+                    if( err ) { res.json({ error: err }) }
+                    else{
+
+                        // Collection récupérée
+                        res.json(collection);
+                    };
+                });
+            };
+
+            // Fermer la connexion
+            db.close();
+        });
     });
 //
 
